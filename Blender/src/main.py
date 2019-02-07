8f78cdea86bfb379e49bb167fb415ce7c3185f46
mansisaksson <mans.95.isaksson@gmail.com>
2019-02-07 23:46:49 +0100

    Example code

--- Blender/src/main.py
+++ Blender/src/main.py
@@ -1,10 +1,27 @@
 import bpy
-from bpy import context
+
 
 def main():
-    obj = context.selected_objects
-    vertices = obj.data.vertices
-    
-    print(vertices[0])
+    for item in bpy.context.selected_objects:
+        print(item.name)
+        if item.type == 'MESH':
+            for vertex in item.data.vertices:
+                print(vertex.co)
+
+    #obj = bpy.context.selected_objects[0]  # type: bpy.types.Object
+    #obj = bpy.data.objects[1]  # type: bpy.types.Object
+
+    current_obj = bpy.context.active_object
+
+    print("=" * 40)  # printing marker
+    for face in current_obj.data.polygons:
+        verts_in_face = face.vertices[:]
+        print("face index", face.index)
+        print("normal", face.normal)
+        for vert in verts_in_face:
+            local_point = current_obj.data.vertices[vert].co
+            world_point = current_obj.matrix_world * local_point
+            print("vert", vert, " vert co", world_point)
+
 
 main()
