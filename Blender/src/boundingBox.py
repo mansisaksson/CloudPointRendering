c6e88196a0076fa2b62e5c6db9b3ae556e7a13e2
mansisaksson <mans.95.isaksson@gmail.com>
2019-02-08 17:40:40 +0100

    some testing

--- Blender/src/boundingBox.py
+++ Blender/src/boundingBox.py
@@ -1,29 +1,30 @@
+from mathutils import Vector
 from typing import List, Set, Dict, Tuple, Optional
 
 
 class BoundingBox:
     def __init__(self):
-        self.min = (0.0, 0.0, 0.0)  # type: Tuple[float, float, float]
-        self.max = (0.0, 0.0, 0.0)  # type: Tuple[float, float, float]
-        self.size = (0.0, 0.0, 0.0)  # type: Tuple[float, float, float]
+        self.min = (0.0, 0.0, 0.0)  # type: Vector
+        self.max = (0.0, 0.0, 0.0)  # type: Vector
+        self.size = (0.0, 0.0, 0.0)  # type: Vector
 
-    def __init__(self, min, max, size):
-        self.min = min  # type: Tuple[float, float, float]
-        self.max = max  # type: Tuple[float, float, float]
-        self.size = size  # type: Tuple[float, float, float]
+    def __init__(self, min: Vector, max: Vector, size: Vector):
+        self.min = min  # type: Vector
+        self.max = max  # type: Vector
+        self.size = size  # type: Vector
 
 
 def create_box(location: List[Tuple[float, float, float]]) -> BoundingBox:
-    loc_min = (0, 0, 0)
-    loc_max = (0, 0, 0)
+    loc_min = Vector((0, 0, 0))
+    loc_max = Vector((0, 0, 0))
     for loc in location:
         # print(loc[2])
         if loc[0] < loc_min[0] or loc[1] < loc_min[1] or loc[2] > loc_min[2]:
-            loc_min = loc
+            loc_min = Vector(loc)
         elif loc[0] > loc_min[0] or loc[1] > loc_min[1] or loc[2] < loc_min[2]:
             loc_max = loc
 
-    size = (loc_max[0] - loc_min[0], loc_max[1] - loc_min[1], loc_min[2] - loc_max[2])
+    size = Vector((loc_max[0] - loc_min[0], loc_max[1] - loc_min[1], loc_min[2] - loc_max[2]))
     box = BoundingBox(loc_min, loc_max, size)
     return box
 
--- Blender/src/main.py
+++ Blender/src/main.py
@@ -2,6 +2,7 @@ import bpy
 import sys
 import os
 import numpy as np
+from mathutils import Vector
 from typing import List, Set, Dict, Tuple, Optional
 
 # change import path to path of blend-file
@@ -16,15 +17,15 @@ def vector3_to_str(vector: Tuple[float, float, float]):
     return "(" + str(vector[0]) + "," + str(vector[1]) + "," + str(vector[2]) + ")"
 
 
-def generate_voxel_data(mesh: bpy.types.Mesh):
+def generate_voxel_data(mesh: bpy.types.Object):
     voxel_size = 0.5
     box = boundingBox.create_box(mesh.bound_box)
 
-    grid_dimensions = (int(box.size[0] / voxel_size),
-                       int(box.size[1] / voxel_size),
-                       int(box.size[2] / voxel_size))
+    grid_dimensions = (int(box.size.x / voxel_size),
+                       int(box.size.y / voxel_size),
+                       int(box.size.z / voxel_size))
 
-    print("grid_dimensions" + vector3_to_str(box.size))
+    print("grid_dimensions" + vector3_to_str(box.size.to_tuple()))
     voxel_array = np.zeros(
         (grid_dimensions[0], grid_dimensions[1], grid_dimensions[2]),
         dtype=[
@@ -48,31 +49,68 @@ def generate_voxel_data(mesh: bpy.types.Mesh):
     for zc in range(0, grid_dimensions[2]):
         for yc in range(0, grid_dimensions[1]):
             for xc in range(0, grid_dimensions[0]):
-                voxel_array[xc, yc, zc]['location'] = ((voxel_size * xc) + box.min[0], (voxel_size * yc) + box.min[1], (voxel_size * zc) - box.min[2])
+                voxel_location = Vector((
+                    (voxel_size * xc) + box.min.x,
+                    (voxel_size * yc) + box.min.y,
+                    (voxel_size * zc) - box.min.z)
+                )
+                result = mesh.closest_point_on_mesh(voxel_location, voxel_size)  # type: Tuple[bool, Vector, Vector, int]
 
+                voxel_array[xc, yc, zc]['location'] = (voxel_location.x, voxel_location.y, voxel_location.z)
+                voxel_array[xc, yc, zc]['normal'] = (result[1].x, result[1].y, result[1].z)
 
     print(voxel_array)
-
-    return
-
-    polygons = mesh.data.polygons  # type: bpy.types.Mesh.polygons
-    for face in polygons:
-        vertices = face.vertices  # type: List[bpy.types.MeshVertex]
-
-        for vert in vertices:
-            local_point = mesh.data.vertices[vert].co
-            world_point = mesh.matrix_world * local_point
-            #print("vert", vert, " vert co", world_point)
+    return voxel_array
 
 
 def main():
     for obj in bpy.context.selected_objects:
         print("Found Selected Object: " + obj.name)
         if obj.type == 'MESH':
-            generate_voxel_data(obj)
+            # generate_voxel_data(obj)
 
-    # obj = bpy.context.selected_objects[0]  # type: bpy.types.Object
-    # obj = bpy.data.objects[1]  # type: bpy.types.Object
+            # img = bpy.data.images[0]  # type: bpy.types.Image
+
+            # for pixel in img.pixels:
+            #     print(pixel)
+
+            # for uvface in bpy.context.object.data.uv_textures.active.data:
+            #    uvface.image = img
+            # texture_coords.py
+
+            obj = bpy.context.active_object
+            mesh = obj.data
+
+            is_editmode = (obj.mode == 'EDIT')
+            if is_editmode:
+                bpy.ops.object.mode_set(mode='OBJECT', toggle=False)
+
+            uvtex = mesh.uv_textures.active
+            print("test start/n")
+            # adjust UVs
+            count = 0
+            for i, uv in enumerate(uvtex.data):
+                print(uvtex)
+                uvs = uv.uv1, uv.uv2, uv.uv3, uv.uv4
+
+                for j, v_idx in enumerate(mesh.faces[i].vertices):
+                    # Face index matches with uvtex.data index, 4 uvs belong to this face
+                    # v_idx points to that specific face vertices
+                    # vertex index starts from 0 and uvs also from 0, they match
+                    # Vertex UV coords are found
+                    print(uv)
+                    print(v_idx)
+                    print(j)
+                    print('/n')
+
+                #    print(uvs[0])
+                #    print(uvs[1])
+                #    print(uvs[2])
+                #    print(uvs[3])
+                #    print("/n")
+                count += 1
+            print("test end/n")
+            print("uv coords count: " + str(count))
 
 
 main()
