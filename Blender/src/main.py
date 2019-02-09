e10a05a280ce89a90955d3c72698497e2ae69233
mansisaksson <mans.95.isaksson@gmail.com>
2019-02-10 00:51:37 +0100

    Code re-structuring

--- Blender/src/main.py
+++ Blender/src/main.py
@@ -13,110 +13,7 @@ if dir not in sys.path:
 import src.boundingBox as boundingBox
 
 
-def generate_voxel_data(mesh: bpy.types.Object):
-    voxel_size = 0.05
-    box = boundingBox.create_box(mesh.bound_box)
-
-    grid_dimensions = (int(box.size.x / voxel_size),
-                       int(box.size.y / voxel_size),
-                       int(box.size.z / voxel_size))
-
-    print("grid_dimensions: " + str(grid_dimensions))
-    voxel_array = np.zeros(
-        (grid_dimensions[0], grid_dimensions[1], grid_dimensions[2]),
-        dtype=[
-            ('location', [
-                ('x', 'int32'),
-                ('y', 'int32'),
-                ('z', 'int32')
-            ]),
-            ('normal', [
-                ('x', 'uint8'),
-                ('y', 'uint8'),
-                ('z', 'uint8')
-            ]),
-            ('color', [
-                ('r', 'uint8'),
-                ('g', 'uint8'),
-                ('b', 'uint8')
-            ])
-        ])
-
-    for zc in range(0, grid_dimensions[2]):
-        for yc in range(0, grid_dimensions[1]):
-            for xc in range(0, grid_dimensions[0]):
-                voxel_location = Vector((
-                    (voxel_size * xc) + box.min.x,
-                    (voxel_size * yc) + box.min.y,
-                    (voxel_size * zc) - box.min.z)
-                )
-                result = mesh.closest_point_on_mesh(voxel_location, voxel_size)  # type: Tuple[bool, Vector, Vector, int]
-
-                # TODO: No need to save location, although it's convenient for debugging
-                voxel_array[xc, yc, zc]['location'] = (voxel_location.x, voxel_location.y, voxel_location.z)
-                if result[0]:
-                    voxel_array[xc, yc, zc]['normal'] = (result[1].x * 255, result[1].y * 255, result[1].z * 255)
-                    voxel_array[xc, yc, zc]['color'] = (255, 255, 255)  # No point in adding color atm, better to focus on render algorithm
-                else:
-                    voxel_array[xc, yc, zc]['normal'] = (0.0, 0.0, 0.0)
-                    voxel_array[xc, yc, zc]['color'] = (0, 0, 0)
-
-    return grid_dimensions, voxel_array
-
-
-def generate_search_tree(voxel_data):
-    dimensions = voxel_data[0]
-    voxel_array = voxel_data[1]
-    layers = []
-
-    bottom_layer = np.zeros(
-        (dimensions[0], dimensions[1], dimensions[2]),
-        dtype=[
-            ('normal', [
-                ('x', 'uint8'),
-                ('y', 'uint8'),
-                ('z', 'uint8')
-            ]),
-            ('color', [
-                ('r', 'uint8'),
-                ('g', 'uint8'),
-                ('b', 'uint8')
-            ]),
-            ('child_0', 'uint32'),
-            ('child_1', 'uint32'),
-            ('child_2', 'uint32'),
-            ('child_3', 'uint32'),
-            ('child_4', 'uint32'),
-            ('child_5', 'uint32'),
-            ('child_6', 'uint32'),
-            ('child_7', 'uint32')
-        ])
-    for x in range(0, dimensions[0]):
-        for y in range(0, dimensions[1]):
-            for z in range(0, dimensions[2]):
-                bottom_layer[x, y, z]['color'] = voxel_array[x, y, z]['color']
-                bottom_layer[x, y, z]['normal'] = voxel_array[x, y, z]['normal']
-                # children will automatically be filled as 0
-
-    generate_node_layers_recursive(layers, bottom_layer)
-
-
-def generate_node_layers_recursive(layers: List, previous_layer):
-    layer = generate_new_layer(previous_layer)
-    print(layer.shape)
-    if layer.shape[0] > 4 and layer.shape[1] > 4 and layer.shape[2] > 4:
-        layers.append(layer)
-        generate_node_layers_recursive(layers, layer)
-
-
-def generate_new_layer(previous_layer):
-    child_base = 2
-    children_per_node = child_base ** 3  # 8 or the number of children per node
-
-    dimensions = [int(previous_layer.shape[0] / children_per_node),
-                  int(previous_layer.shape[1] / children_per_node),
-                  int(previous_layer.shape[2] / children_per_node)]
-
+def generate_search_tree(mesh: bpy.types.Object):
     node_structure = np.dtype([
         ('normal', [
             ('x', 'uint8'),
@@ -138,51 +35,118 @@ def generate_new_layer(previous_layer):
         ('child_7', 'uint32')
     ])
 
-    new_layer = np.zeros((dimensions[0], dimensions[1], dimensions[2]), node_structure)
+    children_per_node = 2 ** 3  # needs to be ^3 TODO: need to make sure the voxel size fits with the child count
+    voxel_size = 0.05
 
-    for x in range(0, dimensions[0], child_base):
-        for y in range(0, dimensions[1], child_base):
-            for z in range(0, dimensions[2], child_base):
-                child_nodes = [
-                    previous_layer[x + 0, y + 0, z + 0],
-                    previous_layer[x + 0, y + 1, z + 0],
-                    previous_layer[x + 1, y + 0, z + 0],
-                    previous_layer[x + 1, y + 1, z + 0],
+    def generate_bottom_layer():
+        box = boundingBox.create_box(mesh.bound_box)
 
-                    previous_layer[x + 0, y + 0, z + 1],
-                    previous_layer[x + 0, y + 1, z + 1],
-                    previous_layer[x + 1, y + 0, z + 1],
-                    previous_layer[x + 1, y + 1, z + 1]
-                ]
+        # TODO: the dimentions needs to be x^4
+        grid_dimensions = (int(box.size.x / voxel_size),
+                           int(box.size.y / voxel_size),
+                           int(box.size.z / voxel_size))
 
-                xc = x / children_per_node
-                yc = y / children_per_node
-                zc = z / children_per_node
+        print("Generating bottom layer with dimensions: " + str(grid_dimensions))
+        voxel_array = np.zeros((grid_dimensions[0], grid_dimensions[1], grid_dimensions[2]), node_structure)
+        for zc in range(0, grid_dimensions[2]):
+            for yc in range(0, grid_dimensions[1]):
+                for xc in range(0, grid_dimensions[0]):
+                    voxel_location = Vector((
+                        (voxel_size * xc) + box.min.x,
+                        (voxel_size * yc) + box.min.y,
+                        (voxel_size * zc) - box.min.z)
+                    )
+                    result = mesh.closest_point_on_mesh(voxel_location,
+                                                        voxel_size)  # type: Tuple[bool, Vector, Vector, int]
 
-                # previous_layer.__array_interface__['data'] returns (pointer, read_only_flag)
-                array_memory_addr = previous_layer.__array_interface__['data'][0]
-                for i, child_node in enumerate(child_nodes):
-                    child = child_nodes[i]
-                    child_memory_addr = child.__array_interface__['data'][0]
+                    if result[0]:
+                        voxel_array[xc, yc, zc]['normal'] = (result[1].x * 255, result[1].y * 255, result[1].z * 255)
+                        voxel_array[xc, yc, zc]['color'] = (255, 255, 255)  # No point in adding color atm, better to focus on render algorithm
+                    else:
+                        voxel_array[xc, yc, zc]['normal'] = (0, 0, 0)
+                        voxel_array[xc, yc, zc]['color'] = (0, 0, 0)
 
-                    # store the memory offset needed to find the child in the array
-                    new_layer[xc, yc, zc]['child_' + str(i)] = (child_memory_addr - array_memory_addr)
+                    # No need to save child data, as bottom layer has no children
 
-            new_layer[xc, yc, zc]['color'] = (0, 0, 0)  # TODO: get avg color from children
-            new_layer[xc, yc, zc]['normal'] = (0.0, 0.0, 0.0)  # TODO: get avg normal from children
+        return voxel_array
 
-    return new_layer
+    bottom_layer = generate_bottom_layer()
+    layers = [bottom_layer]
+
+    def generate_node_layers_recursive(previous_layer):
+        def generate_new_layer():
+            child_base = int(children_per_node ** (1. / 3))
+
+            dimensions = [int(previous_layer.shape[0] / children_per_node),
+                          int(previous_layer.shape[1] / children_per_node),
+                          int(previous_layer.shape[2] / children_per_node)]
+
+            new_layer = np.zeros((dimensions[0], dimensions[1], dimensions[2]), node_structure)
+
+            for x in range(0, dimensions[0], child_base):
+                for y in range(0, dimensions[1], child_base):
+                    for z in range(0, dimensions[2], child_base):
+                        child_nodes = [
+                            previous_layer[x + 0, y + 0, z + 0],
+                            previous_layer[x + 0, y + 1, z + 0],
+                            previous_layer[x + 1, y + 0, z + 0],
+                            previous_layer[x + 1, y + 1, z + 0],
+
+                            previous_layer[x + 0, y + 0, z + 1],
+                            previous_layer[x + 0, y + 1, z + 1],
+                            previous_layer[x + 1, y + 0, z + 1],
+                            previous_layer[x + 1, y + 1, z + 1]
+                        ]
+
+                        xc = x / children_per_node
+                        yc = y / children_per_node
+                        zc = z / children_per_node
+
+                        color_sum = [0, 0, 0]
+                        normal_sum = [0, 0, 0]
+
+                        # previous_layer.__array_interface__['data'] returns (pointer, read_only_flag)
+                        array_memory_addr = previous_layer.__array_interface__['data'][0]
+                        for i, child_node in enumerate(child_nodes):
+                            child = child_nodes[i]
+
+                            color_sum[0] += child['color']['r']
+                            color_sum[1] += child['color']['g']
+                            color_sum[2] += child['color']['b']
+
+                            normal_sum[0] += child['normal']['x']
+                            normal_sum[1] += child['normal']['y']
+                            normal_sum[2] += child['normal']['z']
+
+                            # store the memory offset needed to find the child in the array
+                            child_memory_addr = child.__array_interface__['data'][0]
+                            new_layer[xc, yc, zc]['child_' + str(i)] = (child_memory_addr - array_memory_addr)
+
+                        new_layer[xc, yc, zc]['color'] = (color_sum[0] / len(child_nodes),
+                                                          color_sum[1] / len(child_nodes),
+                                                          color_sum[2] / len(child_nodes))
+                        new_layer[xc, yc, zc]['normal'] = (normal_sum[0] / len(child_nodes),
+                                                           normal_sum[1] / len(child_nodes),
+                                                           normal_sum[2] / len(child_nodes))
+
+            return new_layer
+
+        layer = generate_new_layer()
+        print(layer.shape)
+        layers.append(layer)
+        if layer.shape[0] > 1 and layer.shape[1] > 1 and layer.shape[2] > 1:
+            generate_node_layers_recursive(layer)
+
+    generate_node_layers_recursive(bottom_layer)
+    return layers
 
 
 def main():
     for obj in bpy.context.selected_objects:
         print("Found Selected Object: " + obj.name)
         if obj.type == 'MESH':
-            voxel_data = generate_voxel_data(obj)
-            # print(voxel_data)
-
-            layers = generate_search_tree(voxel_data)
-            print (layers)
+            layers = generate_search_tree(obj)
+            # print(layers)
             return
 
             path = dir + "\\export\\"
